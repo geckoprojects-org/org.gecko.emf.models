beeaf40942edc396f94cfe6474b74027a8f45188
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2025-11-28 15:03:40 +0100

    Fix License
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- io.cloudevents.model/src/io/cloudevents/model/ce/CloudEventsPackage.java
+++ io.cloudevents.model/src/io/cloudevents/model/ce/CloudEventsPackage.java
@@ -1,4 +1,15 @@
 /*
+ * Copyright (c) 2012 - 2025 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *      Mark Hoffmann - initial API and implementation
  */
 package io.cloudevents.model.ce;
 
@@ -26,7 +37,7 @@ import org.osgi.annotation.versioning.ProviderType;
  * @see io.cloudevents.model.ce.CloudEventsFactory
  * @model kind="package"
  *        annotation="Version value='1.0'"
- *        annotation="http://www.eclipse.org/emf/2002/GenModel complianceLevel='17.0' oSGiCompatible='true' basePackage='io.cloudevents.model' resource='XMI'"
+ *        annotation="http://www.eclipse.org/emf/2002/GenModel complianceLevel='17.0' oSGiCompatible='true' basePackage='io.cloudevents.model' resource='XMI' copyrightText='Copyright (c) 2012 - 2025 Data In Motion and others.\nAll rights reserved. \n\nThis program and the accompanying materials are made\navailable under the terms of the Eclipse Public License 2.0\nwhich is available at https://www.eclipse.org/legal/epl-2.0/\n\nSPDX-License-Identifier: EPL-2.0\n\nContributors:\n     Mark Hoffmann - initial API and implementation'"
  * @generated
  */
 @ProviderType
--- io.cloudevents.model/src/io/cloudevents/model/ce/impl/CloudEventsPackageImpl.java
+++ io.cloudevents.model/src/io/cloudevents/model/ce/impl/CloudEventsPackageImpl.java
@@ -1,4 +1,15 @@
 /**
+ * Copyright (c) 2012 - 2025 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *      Mark Hoffmann - initial API and implementation
  */
 package io.cloudevents.model.ce.impl;
 
@@ -369,7 +380,8 @@ public class CloudEventsPackageImpl extends EPackageImpl implements CloudEventsP
 			   "complianceLevel", "17.0",
 			   "oSGiCompatible", "true",
 			   "basePackage", "io.cloudevents.model",
-			   "resource", "XMI"
+			   "resource", "XMI",
+			   "copyrightText", "Copyright (c) 2012 - 2025 Data In Motion and others.\nAll rights reserved. \n\nThis program and the accompanying materials are made\navailable under the terms of the Eclipse Public License 2.0\nwhich is available at https://www.eclipse.org/legal/epl-2.0/\n\nSPDX-License-Identifier: EPL-2.0\n\nContributors:\n     Mark Hoffmann - initial API and implementation"
 		   });
 		addAnnotation
 		  (cloudEventEClass,
